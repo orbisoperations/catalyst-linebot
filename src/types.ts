92a8c5b9041541bcb52672f34178815aac54dd0e
Jesse Taylor <j.taylor@orbisops.com>
2024-05-02 15:49:51 -0700

    line bot just needs graphql

--- src/types.ts
+++ src/types.ts
@@ -59,9 +59,29 @@ export const LinebotFollowEvent = z.object({
 
 export type LinebotFollowEvent = z.infer<typeof LinebotFollowEvent>
 
+export const LinebotPostbackEvent = z.object({
+	replyToken: z.string(),
+	type: z.enum(["postback"]),
+	mode: z.enum(["active"]),
+	webhookEventId: z.string(),
+	deliveryContext: z.object({
+		isRedelivery: z.boolean()
+	}),
+	timestamp: z.number(),
+	source: z.object({
+		type: z.enum(["user"]),
+		userId: z.string()
+	}),
+	postback: z.object({
+		data: z.string()
+	})
+})
+
+export type LinebotPostbackEvent = z.infer<typeof LinebotPostbackEvent>
+
 export const LinebotEvent = z.object({
 	destination: z.string(),
-	events: z.union([LinebotMessageEvent, LinebotUnfollowEvent, LinebotFollowEvent]).array()
+	events: z.union([LinebotMessageEvent, LinebotUnfollowEvent, LinebotFollowEvent, LinebotPostbackEvent]).array()
 })
 
 export type LinebotEvent = z.infer<typeof LinebotEvent>
@@ -72,21 +92,48 @@ const LineBotTextMessage = z.object({
 		text: z.string()
 })
 
+const button = z.object({
+	type: z.literal("button"),
+	style: z.literal("primary"),
+	action: z.object({
+		type: z.literal("postback"),
+		label: z.literal("Send"),
+		data: z.string(),
+		displayText: z.literal("Send").optional()
+	})
+})
+
+const box = z.object({
+	type: z.literal("box"),
+	layout: z.literal("horizontal").or(z.literal("vertical")),
+	contents: z.union([
+		z.object({
+			type: z.literal("text"),
+			text: z.string()
+		}),
+		button
+	]).array()
+})
+
+
+
+const bubble = z.object({
+	type: z.literal("bubble"),
+	body: box,
+	footer: box.optional()
+})
+
+const carousel = z.object({
+	type: z.literal("carousel"),
+	contents: bubble.array()
+})
+
 const LineBotFlexMessage = z.object({
 	type: z.literal("flex"),
 	altText: z.string(),
-	contents: z.object({
-		type: z.literal("bubble"),
-		body: z.object({
-			type: z.literal("box"),
-			layout: z.literal("horizontal").or(z.literal("vertical")),
-			contents: z.object({
-				type: z.literal("text"),
-				text: z.string()
-			}).array()
-		})
-	})
+	contents: bubble.or(carousel)
 })
+
 export const LinebotSendMessages = z.object({
 	replyToken: z.string().optional(),
 	to: z.string().optional(),
@@ -134,8 +181,59 @@ const newPingCoord = (
 	})
 }
 
+const newPingCarousel = (coords: { title: string, cityName: string, coords: string }[]) => {
+	const bubbles = coords.map(coord => {
+		return {
+			"type": "bubble", // 1
+			"body": {
+				// 2
+				"type": "box", // 3
+				"layout": "vertical", // 4
+				"contents": [
+					{
+						"type": "text", // 6
+						"text":coord.title
+					},
+					{
+						"type": "text", // 6
+						"text": coord.cityName
+					},
+					{
+						"type": "text", // 6
+						"text": coord.coords
+					},
+					{
+						"type": "button",
+						"action": {
+							type: "postback",
+							label: "Send",
+							data: coord.coords,
+						},
+						"style": "primary",
+					}
+				]
+			}
+		}
+	})
+
+	return LineBotFlexMessage.parse({
+		type: "flex",
+		altText: "test",
+		contents: {
+			type: "carousel",
+			contents: bubbles
+		}
+	})
+}
+
 export const pingCards = [
 	newPingCoord("Ping 1", "Kinmen Island", "24.42695125386981, 118.22488092750645"),
 	newPingCoord("Ping 2", "Taipei", "25.033916607697982, 121.565390818944"),
 	newPingCoord("Ping 3", "Kaohsuing", "22.76081208289122, 120.24882050572171")
 ]
+
+export const pingCarousel = newPingCarousel([
+	{title: "Ping 1", cityName: "Kinmen Island", coords: "24.42695125386981, 118.22488092750645"},
+	{title: "Ping 2", cityName: "Taipei", coords: "25.033916607697982, 121.565390818944"},
+	{title: "Ping 3", cityName: "Kaohsuing", coords: "22.76081208289122, 120.24882050572171"},
+])
