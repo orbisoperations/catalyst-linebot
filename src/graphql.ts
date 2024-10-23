59c7937ca3068e8377f7f672d2a01b179c02f3ec
J Taylor <j.taylor@orbisops.com>
2024-10-23 16:21:27 -0700

    updated some logs and values for linebot

--- src/graphql.ts
+++ src/graphql.ts
@@ -5,7 +5,7 @@ import {PingEvent, LineBotState} from "./do"
 const typeDefs = `
 type LinePing {
     city: String!
-    lat: String
+    lat: String!
     lon: String!
     expiry: Float!
     title: String!
@@ -23,12 +23,17 @@ export default createSchema({
         Query: {
             pings: async (_, {}, c: Context) => {
                 const demoSwitch = c.env.DEMO_ACTIVE === "true" ? true : false
-                if (!demoSwitch || !Boolean(c.get('valid'))) return []
+                if (!demoSwitch || !Boolean(c.get('valid'))) {
+									console.error("unauthorized user returning empty array")
+									return []
+								}
                 const id = c.env.LineBotState.idFromName("default")
                 const   stub: DurableObjectStub<LineBotState> = c.env.LineBotState.get(id)
                 const pings: PingEvent[] = await stub.getPostbackData()
 
-								return pings.map(ping => {
+								const videoEvents = await  stub.getVideoData()
+
+								return [...pings.map(ping => {
 									const latlon = ping.latlong.replace(" ","").split(",")
 									return {
 										city: ping.city,
@@ -38,7 +43,17 @@ export default createSchema({
 										expiry: ping.expiry,
 										UID: ping.randomPhrase
 									}
-								})
+								}),
+									... videoEvents.map(video => {
+										return {
+											city: video.location ?? "unknown",
+											title: "Video Upload - " + video.link,
+											lat: video.lat,
+											lon: video.lon,
+											expiry: video.expiry,
+											UID: video.link
+										}
+									})]
             },
             _sdl: () => typeDefs
         }
